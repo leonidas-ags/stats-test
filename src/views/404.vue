65a11c23946fcbbf10a646eb6dfb4fa96a348e37
leonidas-ags <126077933+leonidas-ags@users.noreply.github.com>
2023-02-22 13:16:45 +0000

    clean

--- src/views/404.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div id="404">
-    <b-jumbotron
-      header="404 Seite nicht gefunden!"
-      lead="Versuche es doch mal auf einer anderen Seite...">
-    </b-jumbotron>
-  </div>
-</template>
